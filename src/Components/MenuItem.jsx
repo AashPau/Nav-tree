6e247da1fd4e6506d7b12b425b305ef51922338f
Aash <31497659+AashPau@users.noreply.github.com>
2024-04-12 12:23:17 +1000

    completed functionalities css remaining

--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuList } from "./MenuList";
 
 export const MenuItem = ({ item }) => {
+	const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+	const handleToggleChildren = (getCurrentLabel) => {
+		setDisplayCurrentChildren({
+			...displayCurrentChildren,
+			[getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+		});
+	};
 	return (
 		<li className="">
-			<p>{item.label}</p>
-			{item && item.children && item.children.length > 0 ? (
+			<div>
+				<p>{item.label}</p>
+				{item && item.children && item.children.length ? (
+					<span onClick={() => handleToggleChildren(item.label)}>
+						{displayCurrentChildren[item.label] ? "-" : "+"}
+					</span>
+				) : null}
+			</div>
+			{item &&
+			item.children &&
+			item.children.length > 0 &&
+			displayCurrentChildren[item.label] ? (
 				<MenuList list={item.children} />
 			) : null}
 		</li>
